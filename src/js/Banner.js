172b1fb80580931a1eabe8f38a2c8ac2cbb7a7b0
irelay <irelay@free.fr>
2018-08-20 20:54:20 +0200

    0.0.187 - Màj auto

--- src/js/Banner.js
+++ src/js/Banner.js
@@ -12,6 +12,7 @@
 "use strict"
 
 var React = require('react');
+var createReactClass = require('create-react-class');
 var ReactBootstrap = require('react-bootstrap');
 var {
   Nav,
@@ -23,7 +24,7 @@ var {
   Tooltip
 } = ReactBootstrap;
 
-var Banner = React.createClass({
+var Banner = createReactClass({
 
   getInitialState: function() {
     return {
