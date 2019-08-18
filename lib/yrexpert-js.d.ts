4162ce80fd08f4b18b1991f6c0a275cca06feb1a
irelay <irelay@free.fr>
2019-08-18 20:18:55 +0200

    0.0.188 - Màj auto

---
+++ lib/yrexpert-js.d.ts
@@ -0,0 +1,15 @@
+/*
+
+!----------------------------------------------------------------------------!
+!                                                                            !
+! YRexpert : (Your Yrelay) Système Expert sous Mumps GT.M et GNU/Linux       !
+! Copyright (C) 2001-2015 by Hamid LOUAKED (HL).                             !
+!                                                                            !
+!----------------------------------------------------------------------------!
+
+*/
+
+declare function ewd-xpress-react(props: any): any; export = ewd-xpress-react;
+
+
+
