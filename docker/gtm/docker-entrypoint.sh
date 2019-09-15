0fc700cbb34b84fcbd80b03c21a88897416edf80
irelay <irelay@free.fr>
2019-09-15 16:21:45 +0200

    0.0.188 - Màj auto

--- docker/gtm/docker-entrypoint.sh
+++ docker/gtm/docker-entrypoint.sh
@@ -9,11 +9,19 @@
 
 echo '!----------------------------------------------------------------------------!'
 echo
-echo 'IP = ' `hostname -I`
+echo '  IP = ' `hostname -I`
+echo 
+echo '                       *** Accès en mode terminal ***'
+echo "Pour vous connecter en tant qu'utilisateur utilisant l'instance yrelay (par défaut) :"
+echo '  $ ssh -p 50022 yrelayutil@localhost # mode de passe = util'
+echo
+echo '                          ***Accès en mode WEB***'
+echo "Pour accéder à l'administration du serveur yrexpert-js-srv tapez dans l'URL de votre navigateur le lien suivant : "
+echo '  http://localhost:50080/yrexpert/index.html'
 echo
 echo '!----------------------------------------------------------------------------!'
 
-service ssh restart
-service yrelayyrexpert restart
-service yrelayyrexpert-js restart
+service ssh start
+service yrelayyrexpert start
+service yrelayyrexpert-js start
 bash
